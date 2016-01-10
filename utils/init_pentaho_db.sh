540c3b27654dc2004d9f2826ab5de3535eeb6373
sramazzina <sergio.ramazzina@serasoft.it>
2016-01-11 00:38:26 +0100

    Changed to permit externalization of the Pentaho's PostgreSQL metastore

--- utils/init_pentaho_db.sh
+++ utils/init_pentaho_db.sh
@@ -7,6 +7,18 @@ if [ -f /.pentaho_db_already_initialized ]; then
     exit 0
 fi
 
+DB_HOST=${PENTAHO_DB_PORT_5432_TCP_ADDR:-'localhost'}
+DB_PORT=${PENTAHO_DB_PORT_5432_TCP_PORT:-5432}
+
+echo "Referenced Postgres Host - Host: ${DB_HOST}, Port: ${DB_PORT}"
+
+if [ ${DB_HOST} != 'localhost' ]; then
+	echo "Generating .pgpass file to connect to Postgres Host"
+	echo '${DB_HOST}:${DB_PORT}:*:postgres:${POSTGRES_PWD}' > /var/lib/postgresql/.pgpass
+	chmod 600 /var/lib/postgresql/.pgpass
+	chown postgres:postgres /var/lib/postgresql/.pgpass
+fi
+
 su - postgres -c "psql -f /opt/pentaho/biserver-ce/data/postgresql/create_repository_postgresql.sql"
 su - postgres -c "psql -f /opt/pentaho/biserver-ce/data/postgresql/create_quartz_postgresql.sql"
 su - postgres -c "psql -f /opt/pentaho/biserver-ce/data/postgresql/create_jcr_postgresql.sql" 
